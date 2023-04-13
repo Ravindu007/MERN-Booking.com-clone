27b2307f135b024692715789e431589ca41c3ebc
Ravindu007 <ravindu0504@gmail.com>
2023-04-13 23:17:48 +0530

    basic styles

--- frontend/src/components/HomePage-Components/sec-10/Discover.jsx
+++ frontend/src/components/HomePage-Components/sec-10/Discover.jsx
@@ -9,9 +9,11 @@ const Discover = () => {
         <div className="row">
           <div className="col-12 box">
             <div className="center-part">
-              <h6>Find Your Hotels</h6>
-              <h6>For your next trip</h6>
-              <button className='btn'>Discover Homes</button>
+              <div className="text">
+                <h6>Find Your Hotels</h6>
+                <h6>For your next trip</h6>
+                <button className='btn'>Discover Homes</button>
+              </div>
             </div>
             <img src="./back.png" className='mx-auto d-block img-fluid' />
           </div>
